5211f6cdf3870a451552d63702bf4e5880046f29
Patrikkk <patrikveres@hotmail.com>
2020-05-26 15:49:44 +0200

    Cleaning up BuffIds.cs

--- Permabuffs V2/BuffIDs.cs
+++ Permabuffs V2/BuffIDs.cs
@@ -1,10 +1,62 @@
 ﻿using System.Collections.Generic;
+using Terraria.ID;
 
 namespace Permabuffs_V2
 {
 	public static class BuffIDs
 	{
-		private static List<int> NonPermanentBuffs = new List<int>() { 19, 27, 40, 41, 42, 45, 49, 50, 51, 52, 53, 54, 55, 56, 57, 61, 64, 65, 66, 81, 82, 84, 85, 90, 91, 92, 101, 102, 127, 128, 129, 130, 131, 132, 136, 141, 142, 143, 152, 154, 155, 162, 168, 190, 191, 193, 200, 201, 202 };
-		public static bool IsPermanent(this int buffID) => !NonPermanentBuffs.Contains(buffID);
+		private static List<int> PermanentBuffs = new List<int>() 
+        {
+            BuffID.ShadowOrb,
+            BuffID.FairyBlue,
+            BuffID.PetBunny,
+            BuffID.BabyPenguin,
+            BuffID.PetTurtle,
+            BuffID.BabyEater,
+            BuffID.Pygmies,
+            BuffID.BabySkeletronHead,
+            BuffID.BabyHornet,
+            BuffID.TikiSpirit,
+            BuffID.PetLizard,
+            BuffID.PetParrot,
+            BuffID.BabyTruffle,
+            BuffID.PetSapling,
+            BuffID.Wisp,
+            BuffID.BabyDinosaur,
+            BuffID.BabySlime,
+            BuffID.EyeballSpring,
+            BuffID.BabySnowman,
+            BuffID.PetSpider,
+            BuffID.Squashling,
+            BuffID.BlackCat,
+            BuffID.CursedSapling,
+            BuffID.Rudolph,
+            BuffID.Puppy,
+            BuffID.BabyGrinch,
+            BuffID.FairyRed,
+            BuffID.FairyGreen,
+            BuffID.ZephyrFish,
+            BuffID.BunnyMount,
+            BuffID.PigronMount,
+            BuffID.SlimeMount,
+            BuffID.TurtleMount,
+            BuffID.BeeMount,
+            BuffID.MiniMinotaur,
+            BuffID.UFOMount,
+            BuffID.DrillMount,
+            BuffID.ScutlixMount,
+            BuffID.MagicLantern,
+            BuffID.BabyFaceMonster,
+            BuffID.CrimsonHeart,
+            BuffID.UnicornMount,
+            BuffID.CuteFishronMount,
+            BuffID.SuspiciousTentacle,
+            BuffID.CompanionCube,
+            BuffID.BasiliskMount,
+            BuffID.PetDD2Gato,
+            BuffID.PetDD2Ghost,
+            BuffID.PetDD2Dragon
+        };
+		public static bool IsPermanent(this int buffID) => PermanentBuffs.Contains(buffID);
 	}
 }
